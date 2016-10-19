5aa9879b69256eaa2f63acad10e3208750f1b641
Ademola Oladipo <styccs@gmail.com>
2016-10-19 19:21:39 +0100

    updating indexing

--- src/Services/Index.php
+++ src/Services/Index.php
@@ -30,4 +30,33 @@ class Index
         return $server_output;
     }
 
+    static public function get_data($type) {
+        $ch = curl_init();
+        $url = 'http://localhost:4043/api/elasticsearch/'. $type.'/get';
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $server_output = curl_exec ($ch);
+
+        curl_close ($ch);
+
+        return $server_output;
+    }
+
+    static public function filterBy($type, $params=array()) {
+        $ch = curl_init();
+        $url = 'http://localhost:4043/api/elasticsearch/'. $type.'/create';
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,
+            http_build_query($params));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $server_output = curl_exec ($ch);
+
+        curl_close ($ch);
+
+        return $server_output;
+    }
+
 }
